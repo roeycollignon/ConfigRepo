9ce042de025946d405852952ee053f10020952bc
asafc64 <asafc64@gmail.com>
2017-02-07 08:44:21 +0200

    Update WinScript.ps1

--- WinScript.ps1
+++ WinScript.ps1
@@ -1,6 +1,6 @@
 choco install apache-httpd -y
 
-$confPath = "C:\tools\Apache\conf\httpd.conf"
+$confPath = "C:\tools\Apache\httpd-2.4.20\Apache24\conf\httpd.conf"
 (Get-Content $confPath) -replace '/Apache24', 'C:\tools\Apache\httpd-2.4.20\Apache24' | Set-Content $confPath
 
 Start-Service -displayname "Apache"
