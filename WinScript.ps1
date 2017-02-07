c85fc38f3e94aa419dc817647b7cd686f70bbd9a
asafc64 <asafc64@gmail.com>
2017-02-07 08:54:29 +0200

    Update WinScript.ps1

--- WinScript.ps1
+++ WinScript.ps1
@@ -1,6 +1,1 @@
-choco install apache-httpd -y
-
-$confPath = "C:\tools\Apache\httpd-2.4.20\Apache24\conf\httpd.conf"
-(Get-Content $confPath) -replace '/Apache24', 'C:\tools\Apache\httpd-2.4.20\Apache24' | Set-Content $confPath
-
-Start-Service -displayname "Apache"
+choco install nginx-service -y
